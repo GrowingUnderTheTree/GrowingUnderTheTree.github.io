dff15e3e8796b4dd49156237fcf8f093197d4874
Boi <81169774+GrowingUnderTheTree@users.noreply.github.com>
2021-12-02 20:45:08 +0800

    Added tensorflowjs. Might add some cool stuffs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const tf = require(`@tensorflow/tfjs`);
 function CopyFunction() {
     var releases = "https://github.com/GrowingUnderTheTree/lagscream-spigot-plugin/releases";
   
@@ -7,3 +8,23 @@ function CopyFunction() {
     /* Alert the copied text */
     alert("Copied the text: " + releases);
   }
+
+function Openvisualizer() {
+  tfvis.visor().surface({name: 'Very cool Surface', tab: 'Very cool tab', styles: {
+    height: 500
+}})
+}
+
+function morestuffs() {
+  const data = [
+    { index: 0, value: 50 },
+    { index: 1, value: 100 },
+    { index: 2, value: 150 },
+  ];
+  
+  // Get a surface
+  const surface = tfvis.visor().surface({ name: 'Barchart', tab: 'Charts' });
+  
+  // Render a barchart on that surface
+  tfvis.render.barchart(surface, data, {});
+}
